f6903e86b0ea9a6fa9d0b3cc7232f854fb2b6d5d
karolkew <karolke99@gmail.com>
2022-05-16 09:14:56 +0200

    sms sending tests fix

--- UE/Tests/Application/Mocks/IBtsPortMock.hpp
+++ UE/Tests/Application/Mocks/IBtsPortMock.hpp
@@ -26,6 +26,7 @@ public:
     ~IBtsPortMock() override;
 
     MOCK_METHOD(void, sendAttachRequest, (common::BtsId), (final));
+    MOCK_METHOD(void, sendSms, (common::PhoneNumber to, const std::string &message), (final));
 };
 
 }
