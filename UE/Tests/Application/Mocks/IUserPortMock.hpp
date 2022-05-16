99a515ea6f7e8d94dd86fff679d741c135e10835
karolkew <karolke99@gmail.com>
2022-05-16 14:46:24 +0200

    viewSms tests fix

--- UE/Tests/Application/Mocks/IUserPortMock.hpp
+++ UE/Tests/Application/Mocks/IUserPortMock.hpp
@@ -25,6 +25,10 @@ public:
     MOCK_METHOD(void, showNotConnected, (), (final));
     MOCK_METHOD(void, showConnecting, (), (final));
     MOCK_METHOD(void, showConnected, (), (final));
+    MOCK_METHOD(void, setSmsEditView, (), (final));
+    MOCK_METHOD(void, setSmsListView, (), (final));
+    MOCK_METHOD(void, showNewSms, (), (final));
+    MOCK_METHOD(void, checkUnread, (), (final));
 };
 
 }
